11327d1084b7a1d7e1d9c7ae53f734f99e90efe6
Zen <muhzaini30@gmail.com>
2022-07-18 11:47:02 +0800

    simpan

--- script/afterBuild.ts
+++ script/afterBuild.ts
@@ -1,7 +1,10 @@
 import { readFileSync, writeFileSync } from "fs";
 
 let content = readFileSync("dist/index.html").toString();
-content = content
-  .replace(/<script nomodule/g, "<script")
-  .replace(/type=.module./g, "type=nothing");
+content = content.replace(
+  /<script type="module" crossorigin/g,
+  "<script defer"
+);
+// .replace(/<script nomodule/g, "<script")
+// .replace(/type=.module./g, "type=nothing");
 writeFileSync("dist/index.html", content);
